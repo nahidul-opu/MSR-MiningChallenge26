68a06c4a8bcfb059806bde36198bac243f42d1d6
Md. Nahidul Islam Opu <opu.nahidul@gmail.com>
2025-11-19 14:49:47 -0600

    Prepare for OSS  models

---
+++ Run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=MSR
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+#SBATCH --mail-user=opumni@myumanitoba.ca
+#SBATCH --mail-type=END,FAIL
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:h100:2 
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=80G
+#SBATCH --time=02:30:00
+
+mkdir -p logs
+
+set -euo pipefail
+echo "Job started on $(hostname) at $(date)"
+
+module load StdEnv/2023 cuda/12.2
+module load python/3.11
+
+cd /home/opumni/projects/def-shaiful/opumni
+source venv/msr/bin/activate
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export HF_TOKEN=<token>
+export HF_HOME=$SLURM_TMPDIR/hf_cache
+
+mkdir -p $HF_HOME
+
+
+cd Tangled
+srun python filter.py
+echo "Job finished at $(date)"
---
+++ filter.py
@@ -0,0 +1,94 @@
+# %%
+
+import os
+from tqdm import tqdm
+
+from modules.constants import *
+from modules.utilities import *
+from modules.text_classifiers import *
+
+random_sate = 5
+
+# %%
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
+# %%
+def filter_performance_topics(filename, columns, preprocessor, classifier):
+    df = read_aidev(filename)
+    output_rows = []
+
+    for _, row in tqdm(df.iterrows()):
+        text = ""
+        for col in columns:
+            if isinstance(row[col], str):
+                if text != "":
+                    text += "\n"
+
+                val = row[col]
+                if preprocessor is not None:
+                    val = preprocessor.preprocess(val)
+
+                text += val
+
+        if text and text != "" and classifier.classify(text):
+            output_rows.append(row)
+            try:
+                row["matched_words"] = classifier.get_matches()
+            except:
+                pass
+
+    output_df = pd.DataFrame(output_rows)
+
+    return output_df
+
+
+# %%
+
+filename = FileName.POP_ISSUE
+columns = ["title", "body"]
+
+keywords = set(
+    [
+        "performance",
+        "compile-time-hog",
+        "perf",
+        "hang",
+        "optimization",
+        "responsive",
+        "slow",
+        "speed",
+        "latency",
+        "throughput",
+        "wait",
+        "slow",
+        "fast",
+        "lag",
+        "tim",
+        "minor",
+        "stuck",
+        "instant",
+        "respons",
+        "react",
+        "speed",
+        "latenc",
+        "perform",
+        "throughput",
+        "hang",
+        "memory",
+        "leak",
+    ]
+)
+base_text = ", ".join(keywords)
+prompt = [
+    {
+        "role": "system",
+        "content": "You are text classification agent. Analyze the given text and determine if it talks about software performane. Answer 'performance' or 'non-performance' accordingly.  Performance related text may contain keywords such as: "
+        + ", ".join(keywords),
+    }
+]
+classifier = LLMClassifier(prompt)
+preprocessor = None
+
+df = filter_performance_topics(filename, columns, preprocessor, classifier)
+df.to_csv(os.path.join(OUTPUT_DIR, "llm_filtered.csv"), index=False)
